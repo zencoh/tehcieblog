0fab5e50c84157c0dced7bb7a479b2ce68a7b127
Cole Hartkopp <chsoccer4@outlook.com>
2023-03-22 12:08:48 -0500

    created associations between post, user, and comments

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment')
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+   foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
